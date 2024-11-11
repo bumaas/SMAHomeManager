eae68242c20803cf94aadf959b8dda1ebf612861
bumaas <burkhard.maas@arcor.de>
2024-11-11 10:17:32 +0100

    1.0 build 11
    neu:
    - Variable "SW-Version"
    
    korrigiert:
    - Umstellung auf IPSModuleStrict

--- SMAHomeManagerDevice/module.php
+++ SMAHomeManagerDevice/module.php
@@ -1,7 +1,7 @@
 <?php
 
 declare(strict_types=1);
-class SMAHomeManagerDevice extends IPSModule
+class SMAHomeManagerDevice extends IPSModuleStrict
 {
     private const MODID_MULTICAST_SOCKET          = '{BAB408E0-0A0F-48C3-B14E-9FB2FA81F66A}';
     private const PROP_SHOW_DETAILED_CHANNELS     = 'ShowDetailedChannels';
@@ -355,6 +355,7 @@ class SMAHomeManagerDevice extends IPSModule
             $this->registerChannelVariablesFromList('L2', self::LIST_L2, 500);
             $this->registerChannelVariablesFromList('L3', self::LIST_L3, 700);
         }
+        $this->RegisterVariableString('SW_VERSION', $this->Translate('SW-Version'), '', 1000);
     }
 
     private function registerChannelVariablesFromList(string $prefix, array $list, int $initialPosition): void
@@ -418,20 +419,21 @@ class SMAHomeManagerDevice extends IPSModule
         parent::ApplyChanges();
     }
 
-    public function ReceiveData($JSONString): void
+    public function ReceiveData($JSONString): string
     {
         $data = json_decode($JSONString, true, 512, JSON_THROW_ON_ERROR);
         if ($this->ReadPropertyBoolean(self::PROP_ENTENDED_DEBUG_INFORMATION)) {
             $this->SendDebug(
                 sprintf('%s (%s:%s, %s)', __FUNCTION__, $data['ClientIP'], $data['ClientPort'], $data['DataID']),
-                utf8_decode($data['Buffer']),
+                $data['Buffer'],
                 0
             );
         }
         $this->processData($data['Buffer']);
+        return '';
     }
 
-    public function GetConfigurationForParent()
+    public function GetConfigurationForParent(): string
     {
         return json_encode([
                                'Port'               => 9522,
@@ -444,9 +446,9 @@ class SMAHomeManagerDevice extends IPSModule
                            JSON_THROW_ON_ERROR);
     }
 
-    private function processData(string $buffer): void
+    private function processData(string $hraw): void
     {
-        $hraw = bin2hex(utf8_decode($buffer));
+//        $hraw = '534d4100000402a000000001024c0010606901f5b3b8f0ccb40a28ee00010400000000000001080000000001398f2c280002040000015923000208000000000c6bad6f98000304000000042700030800000000002f2071a8000404000000000000040800000000038a83f1c00009040000000000000908000000000207f7fa40000a04000001592a000a08000000000e32bfa9b0000d0400000003e8000e04000000c36f00150400000000000015080000000002160cf0180016040000006c3d00160800000000041f0b0c98001704000000029d001708000000000014d34c08001804000000000000180800000000019b47ccb8001d040000000000001d08000000000260c687b8001e040000006c45001e0800000000047bb9d668001f040000002c79002004000003b8be00210400000003e80029040000000000002908000000000097f628d0002a0400000077ea002a0800000000053ba6b680002b040000000312002b08000000000044f85090002c040000000000002c080000000000b3e6b70800310400000000000031080000000001087d1bb800320400000077f400320800000000054b48eab8003304000000313f003404000003b7e100350400000003e8003d040000000000003d080000000000be7e2408003e0400000074fd003e08000000000543edc2e8003f040000000000003f08000000000009a76148004004000000018800400800000000016fa7fa38004504000000000000450800000000016556b1a000460400000074ff004608000000000559c793a0004704000000306a004804000003b10900490400000003e890000000020e0d5200000000';
         $this->SendDebug(sprintf('%s (%s)', __FUNCTION__, 'hraw'), $hraw, 0);
 
         //Erkennungsstring
@@ -526,20 +528,22 @@ class SMAHomeManagerDevice extends IPSModule
             } elseif ($id === '90000000') {
                 $len       = 4;
                 $swVersion = substr($hraw, $offset * 2, $len * 2);
+                $swVersion = sprintf(
+                    '%s.%s.%s.%s',
+                    hexdec(substr($swVersion, 0, 2)),
+                    hexdec(substr($swVersion, 2, 2)),
+                    hexdec(substr($swVersion, 4, 2)),
+                    chr(hexdec(substr($swVersion, 6, 2)))
+                );
 
                 $this->SendDebug(
                     sprintf('%s (%s)', __FUNCTION__, 'SW-Version'),
-                    sprintf(
-                        '%s.%s.%s.%s',
-                        hexdec(substr($swVersion, 0, 2)),
-                        hexdec(substr($swVersion, 2, 2)),
-                        hexdec(substr($swVersion, 4, 2)),
-                        hexdec(substr($swVersion, 4, 2))
-                    ),
+                    $swVersion,
                     0
                 );
+                $this->setValue('SW_VERSION', $swVersion);
             } else {
-                trigger_error("$id unbekannt");
+                trigger_error(sprintf('id \'%s\' unbekannt, hraw: %s', $id, $hraw));
                 $finished = true;
             }
             $offset += $len;
